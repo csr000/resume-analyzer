7065dd03737d7af228749e3914f889755a90b3d5
JasmineBabaYara <jassyprincess1@icloud.com>
2023-01-25 00:45:36 -0800

    add pdf layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,9 +7,11 @@ export default function Home() {
   const [formData, setFormData] = useState();
   const [pdfUrl, setPdfUrl] = useState(null);
   const [user, setUser] = useState([]);
-  const fileInput = useRef(null)
+  const fileInput = useRef(null);
+  const [click, setClick] = useState(false);
 
   const postData = () => {
+    setClick(!click);
     const query = new URLSearchParams();
 
     for (const [key, value] of formData.entries()) {
@@ -23,7 +25,7 @@ export default function Home() {
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data);
-        setUser(data)
+        setUser(data);
       })
       .catch((error) => {
         console.error("Error:", error);
@@ -39,22 +41,57 @@ export default function Home() {
   };
 
   return (
-    <div className="homeContainer">
+    <div className="homeContainer pb-10">
       <div className="uploadContainer">
         <BsCloudUpload size={100} color="#483EA8" />
         <div className="upload-files">
           <h3>Drag & drop files or </h3>
-          <input type="file" onChange={handleFileSelect} ref={fileInput} style={{ display: 'none' }} />
-          <button onClick={() => fileInput.current.click()} className="border-0 bg-transparent underline" style={{color:'#483EA8'}}>Browse</button>
+          <input
+            type="file"
+            onChange={handleFileSelect}
+            ref={fileInput}
+            style={{ display: "none" }}
+          />
+          <button
+            onClick={() => fileInput.current.click()}
+            className="border-0 bg-transparent underline"
+            style={{ color: "#483EA8" }}
+          >
+            Browse
+          </button>
         </div>
         <p className="formats">Supported formats: PDF</p>
       </div>
-      <button  onClick={() => postData()} className="font-extrabold rounded-full">
+      <button
+        className="start-btn font-extrabold rounded-full"
+        onClick={() => postData()}
+      >
         START
       </button>
       <h2 className="upload-text">Upload a resume to start</h2>
-      <embed src={pdfUrl} width="500" height="700" type="application/pdf" className="mt-10" />
-      <p>{user}</p>
+      <div className={click ? "nav-menu active" : "nav-menu"}>
+        <div className=" flex flex-col items-center justify-center mt-60">
+          <embed src={pdfUrl} width="900" height="900" type="application/pdf" />
+          <h3 className="font-bold text-3xl mt-10" style={{ color: "#3B2667" }}>
+            Resume Analysis
+          </h3>
+          <div className="w-4/6 items-start">
+            <p className="text-xl">Name - </p>
+            <p className="text-xl mt-2">Mail - </p>
+            <p className="text-xl mt-2">Location - </p>
+            <p className="text-xl mt-2">Education - </p>
+          </div>
+          <div className="w-4/6 items-start">
+            <h4
+              className="font-bold text-2xl mt-10"
+              style={{ color: "#8F8F8F" }}
+            >
+              Relevant Keywords
+            </h4>
+          </div>
+        </div>
+        <p>{user}</p>
+      </div>
     </div>
   );
 }
